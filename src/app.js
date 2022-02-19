33d2aa32cfe5e73f7414841bff2d5e85726ea562
jochlopez <joseignacio@pinflag.cl>
2022-02-19 15:36:25 +0000

    Primer avance, generateNumber correcto falta: generateSymbol y css

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let number = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+let symbol = ["♦", "♥", "♠", "♣"];
+
+let generateRandomNumber = () => {
+  let randomNumber = Math.floor(Math.random() * number.length);
+  return number[randomNumber];
+};
+let generateRandomSymbol = () => {
+  let randomSymbol = Math.floor(Math.random() * number.length);
+  return number[randomSymbol];
+};
+
+window.onload = () => {
+  document.querySelector(".symbol-up").classList.add(generateRandomSymbol());
+  document.querySelector(".card").innerHTML = generateRandomNumber();
+
+  //console.log("Hello");
 };
