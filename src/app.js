effc1bc457834075fd358ad07a3c1f8b0f146e44
jochlopez <joseignacio@pinflag.cl>
2022-02-23 00:24:27 +0000

    Tarea Lista! Cosas para revisar: como no hardcodear la posicion de los elementos dentro del div/span/etc

--- src/app.js
+++ src/app.js
@@ -6,20 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 let number = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-let symbol = ["♦", "♥", "♠", "♣"];
+let symbol = ["diamond", "heart", "spade", "leaf"];
 
 let generateRandomNumber = () => {
   let randomNumber = Math.floor(Math.random() * number.length);
   return number[randomNumber];
 };
 let generateRandomSymbol = () => {
-  let randomSymbol = Math.floor(Math.random() * number.length);
-  return number[randomSymbol];
+  let randomSymbol = Math.floor(Math.random() * symbol.length);
+  return symbol[randomSymbol];
 };
 
 window.onload = () => {
-  document.querySelector(".symbol-up").classList.add(generateRandomSymbol());
-  document.querySelector(".card").innerHTML = generateRandomNumber();
+  let num = document.querySelector(".number");
+  num.innerHTML = generateRandomNumber();
 
-  //console.log("Hello");
+  let sym = generateRandomSymbol();
+  let symup = document.querySelectorAll(".symbol");
+  symup[0].classList.add(sym);
+  let symdown = document.querySelectorAll(".symbol");
+  symdown[1].classList.add(sym);
 };
